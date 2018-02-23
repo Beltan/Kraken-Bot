9d32999ed7c016050d0de3df9b545a3ad72f4e27
Beltan <emr_beltane@hotmail.com>
2018-02-23 23:53:01 +0100

    Add files via upload

--- functions.js
+++ functions.js
@@ -5,27 +5,31 @@ const key = config.key; // API Key
 const secret = config.secret; // API Private Key
 const kraken = new KrakenClient(key, secret);
 
-exports.nextParameter = function(pair, index) {
-    if (store.array.length > 0){
-        store.parameter = store.array[index];
-    }else{
-        functions.csvToArray(pair);
-        store.parameter = store.array[index];
-    }
-}
-
-exports.csvToArray = function(pair) {
-    var stuff = fs.readFileSync('./' + pair + '.csv', 'utf8');
-    store.array = stuff.split(',');
-}
-
-exports.getHistoric = async function(pair) {
+exports.initialize = function(pair) {
     store.pair = pair;
     store.first = pair.substring(0, 3);
     store.second = pair.substring(3, 6);
     store.completePair = 'X' + store.first + 'Z' + store.second;
-    if (fs.existsSync('./LastID' + pair + '.txt')){
-        store.last = fs.readFileSync('./LastID' + pair + '.txt', 'utf8');
+    store.searcher = 'data.result.' + store.completePair;
+}
+
+exports.nextParameter = function(index) {
+    if (store.array.length > 0){
+        store.parameter = store.array[index];
+    }else{
+        functions.csvToArray();
+        store.parameter = store.array[index];
+    }
+}
+
+exports.csvToArray = function() {
+    var stuff = fs.readFileSync('./' + store.pair + '.csv', 'utf8');
+    store.array = stuff.split(',');
+}
+
+exports.getHistoric = async function() {
+    if (fs.existsSync('./LastID' + store.pair + '.txt')){
+        store.last = fs.readFileSync('./LastID' + store.pair + '.txt', 'utf8');
     }
     await functions.historic();
 }
@@ -42,7 +46,6 @@ exports.printResultsHistoric = function(error, data) {
     }
     else {
         store.last = data.result.last;
-        store.searcher = 'data.result.' + store.completePair;
         var tempData = [];
         tempData = eval(store.searcher);
         for (m = 0; m < tempData.length; m++){
@@ -77,21 +80,21 @@ exports.trade = function() {
         functions.buy();
     else if (store.longPosition && sellIncrease >= 5)
         functions.sell();
-    console.log('Your current balance is ' + store.testBalance['USD'] + ' USD and ' + store.testBalance[`XRP`] + ' XRP');
+    console.log('Your current balance is ' + store.testBalance[store.first] + ' ' + store.first + ' and ' + store.testBalance[store.second] + ' ' + store.second);
 }
 
 exports.buy = function() {
-    if (store.spread < 0.2)
+    if (store.spread < 0.25)
         functions.placeBuyOrder();
     else 
         console.log('Failed to buy, spread is too large');
 }
 
 exports.placeBuyOrder = function() {
-    commission = store.testBalance['USD'] * 0.0015;
-    store.testBalance['USD'] = store.testBalance['USD'] - commission;
-    store.testBalance['XRP'] = store.testBalance['USD'] / store.ask;
-    store.testBalance['USD'] = 0;
+    commission = store.testBalance[store.second] * 0.0015;
+    store.testBalance[store.second] = store.testBalance[store.second] - commission;
+    store.testBalance[store.first] = store.testBalance[store.second] / store.ask;
+    store.testBalance[store.second] = 0;
     store.longPosition = true;
     store.buyPrice = store.ask;
     var history = {'type' : 'buy', 'value' : store.buyPrice, 'commission' : commission, 'balance' : store.testBalance};
@@ -99,12 +102,12 @@ exports.placeBuyOrder = function() {
 }
 
 exports.placeSellOrder = function() {
-    commission = store.testBalance['XRP'] * store.bid * 0.0015;
-    store.testBalance['USD'] = store.testBalance['XRP'] * store.bid - commission;
+    commission = store.testBalance[store.first] * store.bid * 0.0015;
+    store.testBalance[store.second] = store.testBalance[store.first] * store.bid - commission;
     store.longPosition = false;
     var history = {'type' : 'sell', 'value' : store.bid, 'commission' : commission, 'balance' : store.testBalance};
     store.tradeHistory.push(history);
-    store.testBalance['XRP'] = 0;
+    store.testBalance[store.first] = 0;
 }
 
 exports.sell = function() {
--- test.js
+++ test.js
@@ -1,14 +1,18 @@
 functions = require('./functions');
 store = require('./store');
 
+// Initializes pair values
+functions.initialize('XRPUSD');
 
+/*
 // Real time values and test trading for the pair defined in store
 setInterval (functions.trade, 10000);
 setInterval (functions.depth, 10000);
 setInterval (functions.historicInit, 10000);
+*/
 
 // Gets the data of the specified pair and saves it on a .csv file
-// setInterval(function() {functions.getHistoric('XBTUSD');}, 3000);
+//setInterval(functions.getHistoric, 3000);
 
-// Gets the data from the .csv file and saves the asked value of the determined pair in store.parameter
-// functions.nextParameter('XRPUSD', 1);
+// Gets the data from the .csv file and saves the asked value in store.parameter
+// functions.nextParameter(1);
