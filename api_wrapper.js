931dab22e99e38bdd3454f4b059fe636cdbc6b40
martin.dansc <martin.dansc@gmail.com>
2018-02-24 09:44:09 +0100

    api improvements

--- api_wrapper.js
+++ api_wrapper.js
@@ -8,10 +8,16 @@ const kraken = new KrakenClient(key, secret);
 
 var balance = {'euros' : 50, 'XRPUSD' : 0}
 
+var lastPrice = null;
+
 var transactionHistory = [];
 
 module.exports.getBalance = function() {
     return balance;
+}
+
+module.exports.getCurrentOrders = function() {
+    return lastPrice;
 }
 
 module.exports.placeBuyOrder = function(value) {
@@ -24,7 +30,7 @@ module.exports.placeBuyOrder = function(value) {
     transactionHistory.push(history);
 }
 
-module.exportsfunction.placeSellOrder = function(value) {
+module.exports.placeSellOrder = function(value) {
     commission = balance['XRPUSD'] * value * 0.0015;
     balance['euros'] = balance['XRPUSD'] * value - commission;
     balance['XRPUSD'] = 0;
@@ -32,3 +38,25 @@ module.exportsfunction.placeSellOrder = function(value) {
     var history = {'type' : "sell", "value" : value, "commission" : commission, "balance" : balance};
     transactionHistory.push(history);
 }
+
+//Update functions
+
+var callbackUpdatePrice = function(error, data) {
+    if(error != null) {
+        console.log(error);
+        return;
+    }
+
+    lastPrice = {};
+    lastPrice.spread = 100 * (data.result.XXRPZUSD.asks[0][0] - data.result.XXRPZUSD.bids[0][0]) / data.result.XXRPZUSD.asks[0][0];
+    lastPrice.bid = data.result.XXRPZUSD.bids[0][0];
+    lastPrice.ask = data.result.XXRPZUSD.asks[0][0];
+}
+
+function updatePrice() {
+    pair = 'XRPUSD';
+    count = 1;
+    kraken.api('Depth', {pair, count}, callbackUpdatePrice);
+}
+
+setInterval (updatePrice, 10000);
--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    key : "",
-    secret : ""
+    key : "6eIPFTFkyjZtIv5yPH8/msbYt0+7JQID7b2GLqUoLcp35Si/hFixkVYT",
+    secret : "KL1gTEyaik7UYm4ctzpkwGt0TIQJq4myQSdb5XNekM/Qj6rh5P3f0OyZo9RoZFQSf9dpENk+piDJkbxKrROf7A=="
 }
