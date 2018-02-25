5bcc8bf43b2f656e9712e8b4b69d30a5a928d265
Beltan <emr_beltane@hotmail.com>
2018-02-25 09:02:25 +0100

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    key: '6eIPFTFkyjZtIv5yPH8/msbYt0+7JQID7b2GLqUoLcp35Si/hFixkVYT', // API Key
-    secret: 'KL1gTEyaik7UYm4ctzpkwGt0TIQJq4myQSdb5XNekM/Qj6rh5P3f0OyZo9RoZFQSf9dpENk+piDJkbxKrROf7A==' // API Private Key
-}
