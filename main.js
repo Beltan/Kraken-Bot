7e6cf958c4fbeb5af5e61da5cc91a78835062aff
Beltan <emr_beltane@hotmail.com>
2018-02-25 09:01:26 +0100

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-var api = require('./api_wrapper');
-
-var printValue = function() {
-    var price = api.getCurrentOrders();
-    console.log(JSON.stringify(price));
-}
-
-setInterval (printValue, 10000);
