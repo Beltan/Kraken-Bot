78830850c0c82e41a1159549a8f098f0758db767
martin.dansc <martin.dansc@gmail.com>
2018-03-07 17:18:03 +0100

    api wrapper

--- api/api.js
+++ api/api.js
@@ -1,1 +1,7 @@
+const config = require('./../config').api;
+
 const KrakenClient = require('kraken-api');
+const key = config.key; // API Key
+const secret = config.secret; // API Private Key
+const kraken = new KrakenClient(key, secret);
+
--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports.api = {
     key: '6eIPFTFkyjZtIv5yPH8/msbYt0+7JQID7b2GLqUoLcp35Si/hFixkVYT', // API Key
     secret: 'KL1gTEyaik7UYm4ctzpkwGt0TIQJq4myQSdb5XNekM/Qj6rh5P3f0OyZo9RoZFQSf9dpENk+piDJkbxKrROf7A==', // API Private Key
+    simulation: true
 }
 
 module.exports.ia = {
