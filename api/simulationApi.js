46dcd0eb19af58fd660d133b5a4b40398f26ba50
martin.dansc <martin.dansc@gmail.com>
2018-03-15 10:51:39 +0100

    newFunctionsAdded, known bug in the way orders are executed

--- api/simulationApi.js
+++ api/simulationApi.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-var constants = require('./../constants');
+var constants = require('../constants');
 
 var api = {};
 
@@ -12,15 +12,33 @@ var csvToArray = function() {
     api.historic = stuff.split(',');
 }
 
+var getOtherType = function(type) {
+    if(type == constants.placeSell)
+        return constants.placeBuy;
+    else if(type == constants.placeBuy)
+        return constants.placeSell;
+
+    console.log("There is an error in the decision");
+    return null;
+}
+
 // Order functions
-var placeOrder = function(type, price, quantity, extId = null) {
-    api.txid++;
+var placeOrder = function(type, quantity,  price1, price2 = null, extId = null,
+        closePrice1 = null, closePrice2 = null) {
     
-    var order = {txid : api.txid, price : price, quantity : quantity, 
-        extId : extId, type : type};
+    api.txid++;
+
+    var closeOrder = null;
+    if(closePrice1 != null) {
+        closeOrder = {price1 : closePrice1, price2 : closePrice2};
+    }
+    
+    var order = {txid : api.txid, price : price1, price2: price2, volume : quantity, 
+        extId : extId, type : type, closeOrder : closeOrder};
     
     //init values order
-    order.consumed = 0;
+    order.vol_exec = 0;
+    order.state = constants.pending;
     order.createdTime = new Date();
     
     api.openOrders[api.txid] = order;
@@ -35,12 +53,12 @@ var getNextTxidOrder = function(type) {
     
     for(i in api.openOrders) {
         var order = api.openOrders[i];
-        if(type == constants.placeSell) {
+        if(type == constants.placeSell && order.state == constants.pending) {
             if(txid == -1 || order.price < value) {
                 txid = i;
             }
         }
-        else if(type == constants.placeBuy) {
+        else if(type == constants.placeBuy && order.state == constants.pending) {
             if(txid == -1 || order.price > value) {
                 txid = i;
             }
@@ -67,10 +85,16 @@ var processOrder = function(txid, updatedValue) {
     }
 
     // update the order       
-    order.consumed += quantity;
-    if(Math.abs(order.consumed - order.quantity) <= 0.1) {
-        delete api.openOrders[txid];
+    order.vol_exec += quantity;
+    if(Math.abs(order.vol_exec - order.volume) <= 0.1) {
+        api.openOrders[txid].state = constants.closed;
         cont = true;
+        
+        // create the new order if present
+        if(order.closeOrder != null) {
+            placeOrder(getOtherType(order.type), order.volume,
+                order.closeOrder.price1, order.closeOrder.price2, order.extId);
+        }
     }
 
     return {cont, updatedValue};
@@ -103,7 +127,7 @@ var update = function(newValue) {
 var buy = function(order) {
 
     var randomBuy = getRandomInt(1000, 3000);
-    var quantity = Math.min(order.quantity, randomBuy);
+    var quantity = Math.min(order.volume, randomBuy);
 
     var commission = quantity * 0.0015;
     var realQuantity = quantity - commission;
@@ -117,7 +141,7 @@ var buy = function(order) {
 
 var sell = function(order) {
     var randomBuy = getRandomInt(1000, 3000);
-    var quantity = Math.min(order.quantity, randomBuy);
+    var quantity = Math.min(order.volume, randomBuy);
 
     var commission = quantity * order.price * 0.0015;
 
@@ -130,13 +154,19 @@ var sell = function(order) {
 
 // Decision functions: For each decision we declare a function
 var placeDecisionOrder = function(decision) {
-    return placeOrder(decision.type, decision.price, decision.quantity, decision.extId);
+    return placeOrder(decision.type, decision.quantity, decision.price, decision.price2, decision.extId);
+}
+
+var cancelOrder = function(decision) {
+    api.openOrders[decision.txid].state = constants.canceled;
 }
 
 // and then we add the functions to the object
 var executeFunctions = {};
 executeFunctions[constants.placeBuy] = placeDecisionOrder;
 executeFunctions[constants.placeSell] = placeDecisionOrder;
+executeFunctions[constants.cancelBuy] = cancelOrder;
+executeFunctions[constants.cancelSell] = cancelOrder;
 exports.executeFunctions = executeFunctions;
 
 exports.getValues = function() {
--- constants.js
+++ constants.js
@@ -2,4 +2,9 @@ constants = {};
 constants.placeBuy = "placeBuy";
 constants.placeSell = "placeSell";
 
+constants.pending = "pending";
+constants.closed = "closed";
+constants.canceled = "canceled";
+constants.expired = "expired";
+
 module.exports = constants;
