fd01221ce32ea5d03f94085bbeb51b9197f315c9
Beltan <emr_beltane@hotmail.com>
2018-03-24 12:16:30 +0100

    Real api finished

--- api/api.js
+++ api/api.js
@@ -1,50 +1,102 @@
-const config = require('./../config').api;
-
-const KrakenClient = require('kraken-api');
-const key = config.key; // API Key
-const secret = config.secret; // API Private Key
-const kraken = new KrakenClient(key, secret);
+var constants = require('../constants');
 
 // Kraken calls, pending review
 
-function cancelOrder(txid) {
-    kraken.api('CancelOrder', {txid}, results);
-}
-
-function placeOrder() {
-    var pair = 'XRPUSD';
-    var type = 'buy';
-    var ordertype = 'limit';
-    var price = 0.01;
-    var volume = 30;
-    kraken.api('AddOrder', {pair, type, ordertype, price, volume}, results);
-}
-
-function results (error, data) {
-    if(error != null){
-        console.log(error);
-    }else {
-        return data;
+async function processOrder(decision) {
+    var pair = api.pair;
+    var volume = decision.quantity;
+    if (decision.userref != undefined) {
+        var userref = decision.userref;
+    } else {
+        var userref = undefined;
+    }
+    if (decision.price != 'market') {
+        var ordertype = 'limit';
+        var price = decision.price;
+    } else {
+        var ordertype = 'market';
+        var price = undefined;
+    }
+    if (decision.type == cosntants.placeBuy) {
+        var type = 'buy';
+        try {
+            var order = await kraken.api('AddOrder', {pair, type, price, volume, ordertype, userref});
+            return order;
+        }
+        catch (e) {
+            api.errorHistory.push(e);
+        }
+    } else if (decision.type == constants.placeSell) {
+        var type = 'sell';
+        try {
+            var order = await kraken.api('AddOrder', {pair, type, price, volume, ordertype, userref});
+            return order;
+        }
+        catch (e) {
+            api.errorHistory.push(e);
+        }
+    } else if (decision.type == constants.updateBuy) {
+        var type = 'buy';
+        var txid = decision.txid;
+        try {
+            var cancel = await kraken.api('CancelOrder', {txid});
+            if (cancel.result.count == 1) {
+                var order = await kraken.api('AddOrder', {pair, type, price, volume, ordertype, userref});
+                return order;
+            }
+        }
+        catch (e) {
+            api.errorHistory.push(e);
+        }
+    } else if (decision.type == constants.updateSell) {
+        var type = 'sell';
+        var txid = decision.txid;
+        try {
+            var cancel = await kraken.api('CancelOrder', {txid});
+            if (cancel.result.count == 1) {
+                var order = await kraken.api('AddOrder', {pair, type, price, volume, ordertype, userref});
+                return order;
+            }
+        }
+        catch (e) {
+            api.errorHistory.push(e);
+        }
     }
 }
 
-exports.getValues = async function (txid) {
+exports.getValues = async function (keys) {
     var count = 1;
     var pair = api.pair;
+    var txid = keys.join (', ');
     try {
         var response = await kraken.api('Depth', {pair, count});
         var tradeBalances = await kraken.api('Balance', {});
         var orders = await kraken.api('QueryOrders', {txid});
-        var balance = tradeBalances[api.second];
-        var bid = response['result'][api.fullPair]['bids'][0][0];
-        var ask = response['result'][api.fullPair]['asks'][0][0];
+        var balance = Number(tradeBalances[api.second]);
+        var bid = Number(response['result'][api.fullPair]['bids'][0][0]);
+        var ask = Number(response['result'][api.fullPair]['asks'][0][0]);
         var value = (bid + ask) / 2;
         var values = {bid, ask, value, balance, orders};
         return values;
     }
     catch (e) {
         api.errorHistory.push(e);
+        api.getValues(keys);
     }
+}
+
+exports.execute = async function (decision) {
+    if (decision.type == 'standby') {
+        var keys = decision.keys;
+    }
+    else {
+        var order = processOrder(decision);
+        if (order != undefined) {
+            decision.keys.push(order.result.txid);
+            var keys = decision.keys;
+        }
+    }
+    return keys;
 }
 
 exports.initialize = function(pair) {
